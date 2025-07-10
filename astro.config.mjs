cb08d6a8fe470bf796cc8bb081d92561c0aa387b
Jorge Noguera <yoryer@gmail.com>
2025-07-10 01:55:54 -0300

    new sections

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://example.com",
   integrations: [sitemap(), tailwind()],
 });
